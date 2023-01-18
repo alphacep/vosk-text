25cb6fa7dbe68a5f432a61d6914c2d9c52c9a06f
Nickolay Shmyrev <nshmyrev@gmail.com>
2023-01-18 11:29:15 +0100

    Verbalized punctuation

--- vosk_text/text_normalization/en/taggers/tokenize_and_classify_lm.py
+++ vosk_text/text_normalization/en/taggers/tokenize_and_classify_lm.py
@@ -90,7 +90,7 @@ class ClassifyFst(GraphFst):
             os.makedirs(cache_dir, exist_ok=True)
             whitelist_file = os.path.basename(whitelist) if whitelist else ""
             far_file = os.path.join(
-                cache_dir, f"_{input_case}_en_tn_{deterministic}_deterministic{whitelist_file}_lm.far"
+                cache_dir, f"{input_case}_en_tn_{deterministic}_deterministic{whitelist_file}_lm.far"
             )
         if not overwrite_cache and far_file and os.path.exists(far_file):
             self.fst = pynini.Far(far_file, mode='r')['tokenize_and_classify']
